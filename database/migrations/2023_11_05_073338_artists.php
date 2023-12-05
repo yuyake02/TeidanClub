ae5c8dd584c811255e53365da184a979d9690b92
SoraIshihara0113 <119601973+SoraIshihara0113@users.noreply.github.com>
2023-12-05 21:52:46 +0900

    Revert "Feature/migration+seeder"

--- database/migrations/2023_11_05_073338_artists.php
+++ database/migrations/2023_11_05_073338_artists.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArticlesTable extends Migration
+class Artists extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,7 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
-        Schema::create('articles', function (Blueprint $table) {
-            $table->id();
-            $table->integer('vol')->unique();
-            $table->string('title');
-            $table->text('image');
-            $table->timestamps();
-        });
+        //
     }
 
     /**
@@ -29,6 +23,6 @@ class CreateArticlesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('articles');
+        //
     }
 }
--- database/migrations/2023_11_05_073451_artist_content.php
+++ database/migrations/2023_11_05_073451_artist_content.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAdminsTable extends Migration
+class ArtistContent extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,7 @@ class CreateAdminsTable extends Migration
      */
     public function up()
     {
-        Schema::create('admins', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->timestamps();
-        });
+        //
     }
 
     /**
@@ -29,6 +23,6 @@ class CreateAdminsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admins');
+        //
     }
 }
