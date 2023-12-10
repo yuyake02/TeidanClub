dcea1b363fe9951f9fa556b878ba2bfe878f618e
yuyaK <babyshamblespc0@gmail.com>
2023-12-10 11:10:34 +0900

    各リレーションを追記

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Artist extends Model
 {
     use HasFactory;
+
+    public function snsaccounts()
+    {
+        return $this->hasMany(Snsaccount::class);
+    }
+
+    public function sections()
+    {
+        return $this->belongsToMany(Section::class);
+    }
 }
